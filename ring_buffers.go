51f8ee8cb068b5f268ad8a2d01b2a2e13c508768
Yusheng Guo <113574546+yshngg@users.noreply.github.com>
2025-05-27 10:21:46 +0800

    Tweaks

--- ring_buffers.go
+++ ring_buffers.go
@@ -2,38 +2,38 @@ package rb
 
 import "sync"
 
-// RingBuffers collects individual ring buffers by string key.
+// RingBuffers collects ring buffers by string category.
 type RingBuffers[T any] struct {
 	mtx  sync.Mutex
-	cap  int
+	sz   int
 	bufs map[string]*RingBuffer[T]
 }
 
 // NewRingBuffers returns an empty set of ring buffers, each of which will have
-// a maximum capacity of the given cap.
-func NewRingBuffers[T any](cap int) *RingBuffers[T] {
+// a maximum size of sz, or 1, whichever is greater.
+func NewRingBuffers[T any](sz int) *RingBuffers[T] {
 	return &RingBuffers[T]{
-		cap:  cap,
+		sz:   max(1, sz),
 		bufs: map[string]*RingBuffer[T]{},
 	}
 }
 
-// GetOrCreate returns a ring buffer corresponding to the given category string.
-// Once a ring buffer is created in this way, it will always exist.
+// GetOrCreate returns a ring buffer for the given category string. Once a ring
+// buffer is created in this way, it will always exist.
 func (rbs *RingBuffers[T]) GetOrCreate(category string) *RingBuffer[T] {
 	rbs.mtx.Lock()
 	defer rbs.mtx.Unlock()
 
 	rb, ok := rbs.bufs[category]
 	if !ok {
-		rb = NewRingBuffer[T](rbs.cap)
+		rb = NewRingBuffer[T](rbs.sz)
 		rbs.bufs[category] = rb
 	}
 
 	return rb
 }
 
-// GetAll returns all of the ring buffers in the set, grouped by category.
+// GetAll returns all ring buffers by category.
 func (rbs *RingBuffers[T]) GetAll() map[string]*RingBuffer[T] {
 	rbs.mtx.Lock()
 	defer rbs.mtx.Unlock()
@@ -46,20 +46,22 @@ func (rbs *RingBuffers[T]) GetAll() map[string]*RingBuffer[T] {
 	return all
 }
 
-// Resize all of the ring buffers in the set to the new capacity.
-func (rbs *RingBuffers[T]) Resize(cap int) (dropped map[string][]T) {
-	if cap <= 0 {
-		return
+// Resize all of the ring buffers in the set to the new sz, returning all
+// dropped values for each ring buffer by category. If sz <= 0 it's ignored and
+// the method is a no-op.
+func (rbs *RingBuffers[T]) Resize(sz int) (dropped map[string][]T) {
+	if sz <= 0 {
+		return nil
 	}
 
 	rbs.mtx.Lock()
 	defer rbs.mtx.Unlock()
 
-	rbs.cap = cap
+	rbs.sz = sz
 
 	dropped = map[string][]T{}
 	for name, rb := range rbs.bufs {
-		dropped[name] = append(dropped[name], rb.Resize(cap)...)
+		dropped[name] = append(dropped[name], rb.Resize(sz)...)
 	}
 
 	return dropped
